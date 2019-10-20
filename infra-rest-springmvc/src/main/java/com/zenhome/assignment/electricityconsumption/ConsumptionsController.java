a7ced1a3f1e1cdefe7cd36efd505f384fcdb9db9
Jhon Sanchez <johnmontalvan@hotmail.com>
2019-10-20 17:46:29 -0500

    Adding error handling for bad requests

--- infra-rest-springmvc/src/main/java/com/zenhome/assignment/electricityconsumption/ConsumptionsController.java
+++ infra-rest-springmvc/src/main/java/com/zenhome/assignment/electricityconsumption/ConsumptionsController.java
@@ -7,9 +7,14 @@ import com.zenhome.assignment.electricityconsumption.requestmodel.AddCounterCons
 import com.zenhome.assignment.electricityconsumption.rest.payloadmodel.AddCounterConsumptionJsonPayload;
 import com.zenhome.assignment.electricityconsumption.rest.requestparam.ListConsumptionsRequestParams;
 import com.zenhome.assignment.electricityconsumption.rest.viewmodel.VillagesConsumptionReportJson;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriTemplate;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 @RestController
 @RequestMapping
@@ -35,7 +40,12 @@ public class ConsumptionsController {
 
     @PostMapping(RESOURCE_ADD_CONSUMPTION_URI_TEMPLATE)
     public ResponseEntity addCounterConsumption(@RequestBody AddCounterConsumptionJsonPayload payload) {
-        addCounterConsumption.execute(AddCounterConsumptionRequest.of(payload.counterId(),payload.amount()));
+        addCounterConsumption.execute(AddCounterConsumptionRequest.of(payload.counterId(), payload.amount()));
         return ResponseEntity.created(new UriTemplate(RESOURCE_ADD_CONSUMPTION_URI_TEMPLATE).expand()).build();
+    }
+
+    @ExceptionHandler
+    void handleIllegalArgumentException(HttpServletResponse response, IllegalArgumentException e) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value());
     }
 }
